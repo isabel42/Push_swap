dc41d8fb8123627bc0d931a81fc5c62c8434a3ac
isabel42 <106957731+isabel42@users.noreply.github.com>
2023-03-01 17:22:50 +0100

    """

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: itovar-n <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 15:19:15 by itovar-n          #+#    #+#             */
-/*   Updated: 2023/02/28 16:42:19 by itovar-n         ###   ########.fr       */
+/*   Updated: 2023/03/01 17:22:28 by itovar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,27 @@
 int	main(int argc, char **argv)
 {
 	int		value_max;
+	char	**argvbrackets;
 	t_list	*lst_a;
 	t_list	*lst_b;
 	t_listc	*sol;
 
 	if (argc < 2)
 		return (0);
+	if (argc == 2)
+	{
+		argvbrackets = ft_split(argv[1], '"');
+		argv = ft_split(argvbrackets [0], ' ');
+		printf("%s\n", argv[0]);
+	}
 	if (ft_argvok(argv) == 0)
 	{
 		write(2, "Error\n", 6);
 		exit (0);
 	}
+	printf("%s\n", argv[0]);
 	lst_a = ft_lstdef(argc, argv);
+	ft_lstprint(lst_a);
 	lst_b = NULL;
 	sol = NULL;
 	value_max = 10;
